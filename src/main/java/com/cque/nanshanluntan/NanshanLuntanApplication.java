c87ac794f22602ef1f826b1234ff3ed8a9e38e69
ChaiQ <603280349@qq.com>
2023-06-27 10:25:58 +0800

    创建项目

---
+++ src/main/java/com/cque/nanshanluntan/NanshanLuntanApplication.java
@@ -0,0 +1,13 @@
+package com.cque.nanshanluntan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanshanLuntanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NanshanLuntanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cque/nanshanluntan/NanshanLuntanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cque.nanshanluntan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanshanLuntanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
